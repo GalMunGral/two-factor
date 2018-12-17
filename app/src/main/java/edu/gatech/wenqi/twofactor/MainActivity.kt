ca62770e4eb92df39d556a3541c2086190ce7fef
Wenqi He <hewenqi96@gmail.com>
2018-12-17 01:52:48 -0500

    Create first activity

--- app/src/main/java/edu/gatech/wenqi/twofactor/MainActivity.kt
+++ app/src/main/java/edu/gatech/wenqi/twofactor/MainActivity.kt
@@ -1,2 +1,12 @@
 package edu.gatech.wenqi.twofactor
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
