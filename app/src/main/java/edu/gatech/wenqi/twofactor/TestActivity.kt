974c4946de43e7f327f4aff8c3ba282ccc9c24b6
Wenqi He <hewenqi96@gmail.com>
2018-12-17 05:40:40 -0500

    Add Anko dependencies & Test TCP connection

--- app/src/main/java/edu/gatech/wenqi/twofactor/TestActivity.kt
+++ app/src/main/java/edu/gatech/wenqi/twofactor/TestActivity.kt
@@ -3,7 +3,11 @@ package edu.gatech.wenqi.twofactor
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_test.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.Socket
 
 class TestActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +15,21 @@ class TestActivity: AppCompatActivity() {
         setContentView(R.layout.activity_test)
 
         val ipFromInput: String = intent.extras.getString("ip_address")
-        textTestInput.text = ipFromInput
+
+        doAsync {
+            val socket = Socket(ipFromInput, 3000)
+            val inStream = socket.getInputStream()
+            val buffer: ByteArray = ByteArray(1024)
+            inStream.read(buffer, 0, 1024)
+            val s: String = String(buffer)
+            Log.i("doAsync", s)
+
+            uiThread {
+                Log.i("uiThread", "HERE")
+                textTestInput.text = "IP: $ipFromInput\nResponse: $s"
+            }
+        }
+
 
         buttonSend.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND).apply {
