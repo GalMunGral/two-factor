ca755deac251c7320a2c268bb3cc42a2d8664e60
Wenqi He <hewenqi96@gmail.com>
2018-12-24 02:03:24 -0500

    Start implementing FirebaseMessagingService

--- app/src/main/java/edu/gatech/wenqi/twofactor/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/edu/gatech/wenqi/twofactor/services/MyFirebaseMessagingService.kt
@@ -2,10 +2,26 @@ package edu.gatech.wenqi.twofactor.services
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val TAG = "TestFirebase"
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
-    override fun onNewToken(p0: String?) {
-        super.onNewToken(p0)
-        Log.i("TestFirebase", p0)
+    override fun onNewToken(token: String?) {
+        super.onNewToken(token)
+        Log.d(TAG, "Token: $token")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage?) {
+        super.onMessageReceived(message)
+        Log.d(TAG, "From: ${message?.from}")
+
+        message?.notification?.let {
+            Log.d(TAG, "Notification: ${it.title}, ${it.body}")
+        }
+    }
+
+    override fun onDeletedMessages() {
+        super.onDeletedMessages()
     }
 }
