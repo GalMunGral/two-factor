a0f0eb5cb1c006c4e7d0e7d5dd97370f41c9f64b
Wenqi He <hewenqi96@gmail.com>
2019-02-09 01:33:32 -0500

    Save device token to shared preferences

--- app/src/main/java/edu/gatech/wenqi/twofactor/activities/MainActivity.kt
+++ app/src/main/java/edu/gatech/wenqi/twofactor/activities/MainActivity.kt
@@ -2,36 +2,70 @@ package edu.gatech.wenqi.twofactor.activities
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.Service
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import edu.gatech.wenqi.twofactor.R
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.defaultSharedPreferences
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var requestQueue: RequestQueue
+    private lateinit var token: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestQueue = Volley.newRequestQueue(this)
+        token = defaultSharedPreferences.getString(getString(R.string.device_token_key), "") ?: ""
+
+        Log.i("TestFirebase", "Read from shared preferences: $token")
+
         createNotificationChannel()
 
-        buttonTest.setOnClickListener {
-            Log.i("test", "Hello")
-            val input: String = inputTest.text.toString()
-            val intent = Intent(this, TestActivity::class.java).apply {
-                putExtra("ip_address", input)
+        LocalBroadcastManager.getInstance(this).registerReceiver(object: BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.i("TestFirebase",
+                    "Main activity received token update: ${intent?.getStringExtra(getString(R.string.device_token_key))}")
             }
-            startActivity(intent)
+        }, IntentFilter("UPDATE_TOKEN"));
+
+        buttonTest.setOnClickListener {
+            val input: String = inputTest.text.toString()
+            requestQueue.add(JsonObjectRequest(
+                Request.Method.POST,
+                "http://${getString(R.string.server_ip)}:3000/android/device-token",
+                JSONObject().apply {
+                    put("username", "retarded")
+                    put("token", "")
+                },
+                Response.Listener {
+                    Log.i("TestFirebase", "Success!")
+                },
+                Response.ErrorListener {
+                    Log.d("TestFirebase", it.toString())
+                }
+            ))
         }
     }
 
     private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 getString(R.string.default_notification_channel_id),
@@ -39,11 +73,9 @@ class MainActivity : AppCompatActivity() {
                 description = "description"
                 importance = NotificationManager.IMPORTANCE_HIGH
             }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(channel)
         }
     }
+
 
 }
--- app/src/main/java/edu/gatech/wenqi/twofactor/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/edu/gatech/wenqi/twofactor/services/MyFirebaseMessagingService.kt
@@ -1,15 +1,33 @@
 package edu.gatech.wenqi.twofactor.services
 
+import android.content.Intent
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import edu.gatech.wenqi.twofactor.R
+import org.jetbrains.anko.defaultSharedPreferences
 
 const val TAG = "TestFirebase"
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String?) {
         super.onNewToken(token)
-        Log.d(TAG, "Token: $token")
+        Log.d(TAG, "Received FCM token: $token")
+        token.let {
+
+            // Save the token
+            with(defaultSharedPreferences.edit()) {
+                putString(getString(R.string.device_token_key), it)
+                apply()
+                Log.i("TestFirebase", "Save token $it")
+            }
+
+            // Let MainActivity know
+            LocalBroadcastManager.getInstance(this).sendBroadcast(Intent("UPDATE_TOKEN").apply {
+                putExtra(getString(R.string.device_token_key), it)
+            })
+        }
     }
 
     override fun onMessageReceived(message: RemoteMessage?) {
